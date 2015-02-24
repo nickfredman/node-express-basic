5756bb19b26cd054bd9bc451cc266ea340cdbc4d
Nick Fredman <ntfredman@gmail.com>
2015-02-24 11:18:44 -0700

    Prepped for Heroku

--- app.js
+++ app.js
@@ -10,5 +10,6 @@ app.get('/', function(req, res){
     });
 });
 
-app.listen(3000);
-console.log('Server listening at 127.0.0.1:3000');
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Server listening at ' + port);
